fd7d6344a402e31027a982b4158661ef52fca74a
Jack Donovan <jack.donovan@trojans.dsu.edu>
2019-04-16 14:06:42 -0500

    finished intro to OpenAI

--- OpenAIGym/cartPole.py
+++ OpenAIGym/cartPole.py
@@ -1,107 +1,12 @@
-from __future__ import division, print_function, unicode_literals
 import tensorflow as tf
 import numpy as np
 import os
 import sys
 
-def reset_graph(seed=42):
-    tf.reset_default_graph()
-    tf.set_random_seed(seed)
-    np.random.seed(seed)
-
-
+import gym
 import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-plt.rcParams['axes.labelsize'] = 14
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams['ytick.labelsize'] = 12
-
-
-PROJECT_ROOT_DIR = "."
-CHAPTER_ID = "rl"
-
-def save_fig(fig_id, tight_layout=True):
-    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format='png', dpi=300)
-
-
-
-import gym
-
-
-env = gym.make('MsPacman-v0')
-obs = env.reset()
-obs.shape
-img = env.render(mode="rgb_array")
-plt.figure(figsize=(5,4))
-plt.imshow(img)
-plt.axis("off")
-save_fig("MsPacman")
-plt.show()
-
-(img == obs).all()
-
-def plot_environment(env, figsize=(5,4)):
-    plt.close()  # or else nbagg sometimes plots in the previous cell
-    plt.figure(figsize=figsize)
-    img = env.render(mode="rgb_array")
-    plt.imshow(img)
-    plt.axis("off")
-    plt.show()
-
-env.action_space
-
-
-env.reset()
-for step in range(110):
-    env.step(3) #left
-for step in range(40):
-    env.step(8) #lower-left
-
-plot_environment(env)
-obs, reward, done, info = env.step(0)
-obs.shape
-reward
-done
-info
-
-
-frames = []
-
-n_max_steps = 1000
-n_change_steps = 10
-
-obs = env.reset()
-for step in range(n_max_steps):
-    img = env.render(mode="rgb_array")
-    frames.append(img)
-    if step % n_change_steps == 0:
-        action = env.action_space.sample() # play randomly
-    obs, reward, done, info = env.step(action)
-    if done:
-        break
-
-
-def update_scene(num, frames, patch):
-    patch.set_data(frames[num])
-    return patch,
-
-def plot_animation(frames, repeat=False, interval=40):
-    plt.close()  # or else nbagg sometimes plots in the previous cell
-    fig = plt.figure()
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
-
-
-video = plot_animation(frames)
-plt.show()
-
-env.close()
 
 env = gym.make("CartPole-v0")
 obs = env.reset()
